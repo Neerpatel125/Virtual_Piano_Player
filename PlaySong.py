92e5d04cfbfcd33555194f9bbfad6ef0b49dbadc
Neer Patel <neerpatel@neers-imac.myfiosgateway.com>
2025-12-14 00:21:55 -0500

    fix: dash delay timing

--- PlaySong.py
+++ PlaySong.py
@@ -106,7 +106,7 @@ if len(sys.argv) > 2:
 if not isMidi or spaceDelay is None or dashDelay is None:
 	seconds_per_beat = 60.0 / bpm
 	spaceDelay = seconds_per_beat / 4			
-	dashDelay = seconds_per_beat / 16
+	dashDelay = seconds_per_beat / 4
 
 print("Starting...")
 thread = Thread(target=playSong, args=(song,))
